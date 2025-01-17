d396b8826bb4820ad6025ea08f27955fa25aaf0e
alexdev0218 <alexdev0218@gmail.com>
2025-01-17 12:03:00 -0500

    refactoring code

--- util.py
+++ util.py
@@ -13,9 +13,6 @@ def get_env_tg_api_hash():
 def get_env_tg_source_channel():
     return str(os.getenv("TG_SOURCE_CHANNEL"))
 
-def get_env_tg_destination_channel():
-    return str(os.getenv("TG_DESTINATION_CHANNEL"))
-
 def get_dc_bot_token():
     return str(os.getenv("DC_BOT_TOKEN"))
 
